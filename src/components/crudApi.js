53d19efcb8bdea55819ac6d2d4bd5619a349236b
JesusZamora20 <jbermudezamora87@gmail.com>
2023-02-08 00:02:50 -0500

    using the helper fetch to get the data and implementing loader and error message

--- src/components/crudApi.js
+++ src/components/crudApi.js
@@ -1,11 +1,35 @@
 import React, { useState } from 'react';
+import { useEffect } from 'react';
+import { helpHttp } from '../helpers/helpHttp';
 import CrudForm from './crudForm';
 import CrudTable from './crudTable';
+import Loader from './Loader';
+import Message from './Message';
 
 
 function CrudApi(){
-    const [db, setDb] = useState([]);
+    const [db, setDb] = useState(null);
     const [dataToEdit, setDataToEdit] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(null);
+
+    let url = "http://localhost:5000/santos";
+
+    useEffect(() => {
+        setLoading(true);
+        helpHttp().get(url).then(res => {
+            // console.log(res);
+            if(!res.err){
+                setDb(res);
+                setError(null);
+            } else {
+                setDb(null);
+                setError(res);
+            }
+
+            setLoading(false);
+        })
+    },[])
 
     const CreateData = (data) => {
         data.id = Date.now();
@@ -37,10 +61,12 @@ function CrudApi(){
             dataToEdit={dataToEdit}
             setDataToEdit={setDataToEdit}
             />
-            <CrudTable 
+            {loading && <Loader />}
+            {error && <Message msg={`Error ${error.status}: ${error.statusText}`} bgColor="#dc3545"/>}
+            {db && <CrudTable 
             data={db} 
             DeleteData={DeleteData} 
-            setDataToEdit={setDataToEdit} />
+            setDataToEdit={setDataToEdit} />}
         </>
     );
 }
--- src/helpers/helpHttp.js
+++ src/helpers/helpHttp.js
@@ -1,49 +1,57 @@
 export const helpHttp = () => {
     const customFetch = (endpoint, options) => {
-        const defaultHeaders = {
-            accept: "application/json",
-        };
-        /*El metodo abort permite cancelar manualmente cualquier peticion que se haya quedado 
-        ciclada por algun fallo de la API*/
-        const controller = new AbortController();
-        options.signal = controller.signal;
+        const defaultHeader = {
+        accept: "application/json",
+    };
 
-        options.method = options.method || "GET";
-        options.headers = options.headers ? {...defaultHeaders, ...options.headers } : defaultHeaders;
+    const controller = new AbortController();
+    options.signal = controller.signal;
 
-        options.body = JSON.stringify(options.body) || false;
-        if(!options.body) delete options.body;
+    options.method = options.method || "GET";
+    options.headers = options.headers
+        ? { ...defaultHeader, ...options.headers }
+        : defaultHeader;
 
-        let fetchErr  ={
-            err: true,
-            status: res.status || "00",
-            statusText: res.statusText || "An error has occurred"
-        }
+    options.body = JSON.stringify(options.body) || false;
+    if (!options.body) delete options.body;
 
-        console.log(options);
-        setTimeout(() => {
-            controller.abort();
-        },3000);
+      //console.log(options);
+    setTimeout(() => controller.abort(), 3000);
 
-        return fetch(endpoint,options)
-        .then(res => {res.ok ? res.json() : Promise.reject({
-            err: true,
-            status: res.status || "00",
-            statusText: res.statusText || "An error has occurred"})}).catch(err => err);
-    }
-    const get = (url, options = {}) => customFetch(url,options);
+    return fetch(endpoint, options)
+        .then((res) =>
+            res.ok
+            ? res.json()
+            : Promise.reject({
+                err: true,
+                status: res.status || "00",
+                statusText: res.statusText || "An error has occurred",
+            })
+        )
+        .catch((err) => err);
+    };
+
+    const get = (url, options = {}) => customFetch(url, options);
+
     const post = (url, options = {}) => {
         options.method = "POST";
-        return customFetch(url,options);
-    }
-    const put = (url,options) => {
-        options.method = "PUT";
-        return customFetch(url,options);
-    }
-    const del = (url,options) => {
-        options.method = "DELETE";
-        return customFetch(url,options);
-    }
+        return customFetch(url, options);
+    };
 
-    return{get,post,put,del}
-}
+    const put = (url, options = {}) => {
+        options.method = "PUT";
+        return customFetch(url, options);
+    };
+
+    const del = (url, options = {}) => {
+        options.method = "DELETE";
+        return customFetch(url, options);
+    };
+
+    return {
+        get,
+        post,
+        put,
+        del,
+    };
+};
